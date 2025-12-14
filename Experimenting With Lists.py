f237a6904894b6976482629be49464dc37c0f08f
AR-2013 <anairarandeva@icloud.com>
2025-12-14 12:19:51 +0400

    Experimenting with lists

---
+++ Experimenting With Lists.py
@@ -0,0 +1,12 @@
+empty_list = []
+print(empty_list)
+
+number_list = [1, 2, 3, 4, 5]
+print(number_list)
+
+triples = [1, 2, 3] *3
+print(triples)
+
+aList = [500, 400, 300, 200, 100, 0]
+aList = aList[::-1]
+print(aList, "\n")
